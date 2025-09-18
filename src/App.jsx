c89e07454cd36887dbe703762d3662a9a7141202
shoybit <shoyaib105@gmail.com>
2025-09-18 20:52:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Countries from './component/countries/countries'
+function App() {
+
+  return (
+    <>
+    <Countries></Countries>
+    </>
+  )
+}
+
+export default App
